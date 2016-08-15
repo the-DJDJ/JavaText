2e6cc95b21e4bfad97a2904d07351ab72f938223
the-djdj <jenkins.daniel.02@gmail.com>
2016-08-15 22:21:51 +0200

    [new] Added startsWithVowel method
    Added a simple method to check if a string starts with a vowel

--- src/BeanPlant/the_DJDJ/JavaText/util/StringTools.java
+++ src/BeanPlant/the_DJDJ/JavaText/util/StringTools.java
@@ -11,12 +11,38 @@ public class StringTools {
      * A very basic method to capitalise the first letter of a string
      * 
      * @param string the string to capitalise
-     * 
      * @return The same string, with the first letter capitalised
      */
     public static String capitaliseFirstLetter(String string){
         
         return string.substring(0, 1).toUpperCase() + string.substring(1);
+        
+    }
+    
+    /**
+     * A simple boolean that determines whether or not a string starts with a
+     * vowel or not.
+     * 
+     * @param string the string to test
+     * @return whether or not it begins with a vowel.
+     */
+    public static boolean startsWithVowel(String string){
+        
+        if(string.isEmpty()) return false;
+        
+        switch(string.toLowerCase().charAt(0)){
+            
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                return true;
+                
+            default:
+                return false;
+            
+        }
         
     }
     
