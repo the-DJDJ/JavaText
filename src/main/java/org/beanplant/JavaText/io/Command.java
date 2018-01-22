c268b4da619f7bb23b4d971666d599c2a089aba2
the-DJDJ <jenkins.daniel.02@gmail.com>
2018-01-23 00:24:16 +0200

    [fix] Added JavaDoc to Command
    Added the missing documentation to the Command class.

--- src/main/java/org/beanplant/JavaText/io/Command.java
+++ src/main/java/org/beanplant/JavaText/io/Command.java
@@ -4,8 +4,12 @@ import org.beanplant.JavaText.JavaText;
 import org.beanplant.JavaText.world.World;
 
 /**
+ * The interface extended when a user creates a custom command. This is also
+ * used by JavaText itself, and includes the two most commonly used variables in
+ * commands, as well as provides a method that must be overridden in order for
+ * the command to work.
  *
- * @author the-djdj
+ * @author the_DJDJ
  */
 public interface Command {
     
