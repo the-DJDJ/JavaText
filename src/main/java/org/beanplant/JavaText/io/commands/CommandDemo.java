edff89a846bd9204b7679b162920d909e1f75790
the-DJDJ <jenkins.daniel.02@gmail.com>
2018-03-19 16:24:12 +0200

    [new] Created command template
    Created a neat little template for if a developer wants to make their own command in a game

---
+++ src/main/java/org/beanplant/JavaText/io/commands/CommandDemo.java
@@ -0,0 +1,18 @@
+package org.beanplant.JavaText.io.commands;
+
+import org.beanplant.JavaText.io.Command;
+
+/**
+ * The template class for any commands. If you want to write your own, simply
+ * copy and paste this class, and add your code inside of the execute method!
+ *
+ * @author the_DJDJ
+ */
+public class CommandDemo implements Command {
+
+    @Override
+    public void execute(String arguments) {
+        
+    }
+    
+}
