50e0e853a0d0b375468b9dd845e352d015dc3616
the-DJDJ <jenkins.daniel.02@gmail.com>
2018-03-19 12:38:19 +0200

    [new] Started work on the first JavaText game

--- src/main/java/levels/Murder.java
+++ src/main/java/levels/Murder.java
@@ -1,67 +1,57 @@
 package levels;
 
-import org.beanplant.JavaText.npc.Boss;
-import org.beanplant.JavaText.npc.MobileEntity;
-import org.beanplant.JavaText.world.exit.Exit;
 import org.beanplant.JavaText.world.Location;
 import org.beanplant.JavaText.world.World;
-import org.beanplant.JavaText.user.Item;
-import org.beanplant.JavaText.world.exit.Type;
 
 /**
- * The village level. This is a more advanced level, and the first official one
- * to be added to the game. Please don't judge me if it's bad!
  *
- * @author the_djdj
+ * @author the_DJDJ
  */
-public class Village extends World {
+public class Murder extends World {
     
     // Create room objects
-    private final Location l01 = new Location ("Derelict Trainstation", "You stand on the platform of an old, empty trainstation. The tracks are rusty and studded with weeds, and it is obvious that no trains have been through here in a while. Turning around, you glance at the clock which has now become a home for the mice. The absence of its ticking creates an eery, spooky silence, and a breeze draws through the abandonded corridors, pulling your attention east, onto a road leading to the nearby village.");
-    private final Location l02 = new Location ("Village Road", "After walking for a few minutes, you come across a log blocking the road between the village and the train station. On closer inspection, you find that the log appears to have been placed across the road intentionally, acting as a block on the road and halting vehicle access to the village. Is it there to keep something in, or out? The log isn't particularly big and shouldn't be very hard to climb over, but there is also a path that goes around it that seems to lead up a rough dirt track through the trees.");
-    private final Location l03 = new Location ("Forest Pathway", "The breeze dies down as the trees grow thicker here. It's hard to tell if you're imagining it or not, but something seems to be moving slowly through the thicket, and the dusty rays of sunlight are now so scarce that you can barely see your hands. You have no choice but to turn back.");
-    private final Location l04 = new Location ("", "");
-
-    // Create exit objects
-    private final Exit e01 = new Exit (Exit.EAST, l02, Type.PATHWAY);
-    private final Exit e02 = new Exit (Exit.NORTHEAST, l03, Type.PATHWAY);
-    private final Exit e03 = new Exit (Exit.EAST, l04);
-    private final Exit e04 = new Exit (Exit.WEST, l01);
-    private final Exit e05 = new Exit (Exit.SOUTHWEST, l02, Type.PATHWAY);
+    private final Location l001 = new Location ("Prologue", "On a pleasant summer day in London, you decide to visit your mentor, Sherlock Holmes, and your cousin, Dr. John Watson. Perhaps today Mr. Holmes will hand you a case to solve yourself."
+            + " <token:paragraph> Upon reaching 221B Baker Street, Mrs. Hudson admits you with a smile, although obviously in the midst of a busy cleaning session. \"How nice to see you again,\" says Holmes' landlady. \"Mr. Holmes and Doctor Watson are in their rooms. You know the way.\""
+            + " <token:paragraph> \"Yes ma'am,\" you reply, turning toward the stair."
+            + " <token:paragraph> \"You might do me a favor,\" she continues. You pause at the words. \"Dr. Watson's newspaper just came; it would save a trip if you'd take it with you. I just came down from carrying the doctor a telegram, and I've a thousand things to do today.\""
+            + " <token:paragraph> Naturally, you agree. At your knock, Mr. Holmes invites you in, and both men greet you brusquely. Doctor Watson sits at his desk, writing, a telegram sticking out of his vest pocket. His pipe drawing well, Holmes relaxes by the window. After returning their greetings, you hand your cousin his newspaper and sit to talk with Holmes. Although rarely given easily, the detective's thoughts are always worth hearing. As you talk, you notice Watson turn to the back page of his paper, look at it intently for a moment, and then toss it aside with a grunt of disgust."
+            + " <token:paragraph> Holmes chuckles. \"You're quite right, Watson. You will make more money telling the story of Silver Blaze than betting any of tomorrow's races.\""
+            + " <token:paragraph> \"That's what I've decided, Holmes,\" Watson replies. \"Those horses would have trouble - how did you know what I was thinking?\""
+            + " <token:paragraph> \"I knew the same way I always know,\" Holmes replies. \"By observation and reasoning. Besides, Watson, you make so much from my analysis of the trivial that you hardly have the right to complain.\""
+            + " <token:paragraph> \"All right, Holmes, the point is well taken,\" Watson agrees, blushing as he smiles. \"But for the life of me, I don't see how you did it this time. I hadn't even told you that I was writing one of your adventures, much less which case.\""
+            + " <token:paragraph> Suddenly nervous, you marshall your thoughts, trying to remember every significant detail of the morning."
+            + " <token:paragraph> \"Where shall I start?\" you ask, stalling for time."
+            + " <token:paragraph> \"Why don't you try the telegram first?\" Holmes suggests. \"Just before you arrived, Watson received a telegram which he stuck in his pocket without reading. What does that suggest to you?\""
+            + " <token:paragraph> \"That he was in a hurry,\" you reply, covering your mouth in dismay at speaking without thinking. \"No, how could he be in a hurry when he's sitting at his desk? It must be something else.\"");
+    private final Location l323 = new Location ("", "");
+    private final Location l638 = new Location ("", "");
     
-    public Village(){
+    // Create exit objects
+
+    public Murder(){
         
         // Set the title and description of the level
-        setTitle("The Village");
-        setDescription("You received a call a couple of weeks ago about something strange going on in a little village about fifty miles south of you. Something eerie. Newspaper reports haven't shown anything new, but even they cannot be depended on anymore. It's obvious that nobody has entered or left the village in a while, and the roads surrounding it are unnaturally quiet. Your taxi dropped you off here, on the outskirts, as you realise your mistake. Maybe you should've thought this one through a little more...");
+        setTitle("Murder at the Diogenes Club");
+        setDescription("by Gerald Lientz");
         getPlayer().setHealth(100);
         
         // Add items
-        l01.addItem(Item.newspaper, 2);
         
         // Attach exits to locations
-        l01.addExit(e01);
-        l02.addExit(e02);
-        l02.addExit(e03);
-        l02.addExit(e04);
-        l03.addExit(e05);
         
         // Add bosses
-        l02.setBoss(Boss.wombat);
         
-        MobileEntity s = MobileEntity.sheep.place(l02);
+        // Place MobileEntities
         
         // Add an entity or two
-        l02.addEntity(s);
         
         // Add locations to our game lists
-        addLocation (l01);
-        addLocation (l02);
-        addLocation (l03);
-        addLocation (l04);
+        addLocation (l001);
+        addLocation (l323);
+        addLocation (l638);
         
         // Set current location
-        getPlayer().setLocation(l01);
+        getPlayer().setLocation(l001);
         
     }
     
