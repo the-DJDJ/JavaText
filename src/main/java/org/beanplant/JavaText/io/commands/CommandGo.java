c37f20c5a091481e0fbfb122b2ba1fd0a3c58877
the-DJDJ <jenkins.daniel.02@gmail.com>
2018-03-19 16:41:33 +0200

    [new] Added random move ability
    Added the ability for a player to move in a random (valid) direction

--- src/main/java/org/beanplant/JavaText/io/commands/CommandGo.java
+++ src/main/java/org/beanplant/JavaText/io/commands/CommandGo.java
@@ -17,55 +17,64 @@ public class CommandGo implements Command {
     public void execute(String arguments) {
     
         boolean valid = false;
-
-        // Check if the direction is valid
-        for (Exit exit : commandParser.getWorld().getPlayer().getLocation().getExits()) {
+        
+        // Check for possible random exits
+        if(arguments.equals("RANDOM") || arguments.equals("R")) {
             
-            if(arguments.equals(exit.getDirectionName()) || arguments.equals(exit.getShortDirectionName())){
-                
-                if((!commandParser.getWorld().getPlayer().getLocation().hasBoss()) || (commandParser.getWorld().getPlayer().getLocation().hasBoss() && commandParser.getWorld().getPlayer().getLocation().getBoss().isAvoidable())){
-                
-                    if(!exit.isLocked()){
+            this.execute(commandParser.getWorld().getPlayer().getLocation().getExits().get((int) (Math.random() * commandParser.getWorld().getPlayer().getLocation().getExits().size())).getDirectionName());
+            
+        } else {
 
-                        // Set location to the location pointed to by exit
-                        commandParser.getWorld().getPlayer().setLocation(exit.getLeadsTo());
+            // Check if the direction is valid
+            for (Exit exit : commandParser.getWorld().getPlayer().getLocation().getExits()) {
 
-                        // Show new location
-                        commandParser.getWorld().showLocation(false);
+                if(arguments.equals(exit.getDirectionName()) || arguments.equals(exit.getShortDirectionName())){
+
+                    if((!commandParser.getWorld().getPlayer().getLocation().hasBoss()) || (commandParser.getWorld().getPlayer().getLocation().hasBoss() && commandParser.getWorld().getPlayer().getLocation().getBoss().isAvoidable())){
+
+                        if(!exit.isLocked()){
+
+                            // Set location to the location pointed to by exit
+                            commandParser.getWorld().getPlayer().setLocation(exit.getLeadsTo());
+
+                            // Show new location
+                            commandParser.getWorld().showLocation(false);
+
+                        } else {
+
+                            commandParser.getWorld().getOutputStream().printSpaced(exit.getType().getLockedMessage(), WidthLimitedOutputStream.BOTH);
+
+                        }
 
                     } else {
 
-                        commandParser.getWorld().getOutputStream().printSpaced(exit.getType().getLockedMessage(), WidthLimitedOutputStream.BOTH);
+                        if(commandParser.getWorld().getPlayer().getLocation().getBoss().getName().contains(" ")){
+
+                            commandParser.getWorld().getOutputStream().printSpaced(commandParser.getWorld().getMessageBuilder().getMovePlayerAttackedMessage(commandParser.getWorld().getPlayer().getLocation().getBoss().getName().toLowerCase().substring(commandParser.getWorld().getPlayer().getLocation().getBoss().getName().lastIndexOf(" ")), commandParser.getWorld().getPlayer().getLocation().getBoss().getDamage()), WidthLimitedOutputStream.BOTH);
+
+
+                        } else {
+
+                            commandParser.getWorld().getOutputStream().printSpaced(commandParser.getWorld().getMessageBuilder().getMovePlayerAttackedMessage(commandParser.getWorld().getPlayer().getLocation().getBoss().getName().toLowerCase(), commandParser.getWorld().getPlayer().getLocation().getBoss().getDamage()), WidthLimitedOutputStream.BOTH);
+
+                        }
+
+                        commandParser.getWorld().getPlayer().setHealth(commandParser.getWorld().getPlayer().getHealth() - commandParser.getWorld().getPlayer().getLocation().getBoss().getDamage());
 
                     }
-                
-                } else {
-                    
-                    if(commandParser.getWorld().getPlayer().getLocation().getBoss().getName().contains(" ")){
-                        
-                        commandParser.getWorld().getOutputStream().printSpaced(commandParser.getWorld().getMessageBuilder().getMovePlayerAttackedMessage(commandParser.getWorld().getPlayer().getLocation().getBoss().getName().toLowerCase().substring(commandParser.getWorld().getPlayer().getLocation().getBoss().getName().lastIndexOf(" ")), commandParser.getWorld().getPlayer().getLocation().getBoss().getDamage()), WidthLimitedOutputStream.BOTH);
-                    
-                        
-                    } else {
-                        
-                        commandParser.getWorld().getOutputStream().printSpaced(commandParser.getWorld().getMessageBuilder().getMovePlayerAttackedMessage(commandParser.getWorld().getPlayer().getLocation().getBoss().getName().toLowerCase(), commandParser.getWorld().getPlayer().getLocation().getBoss().getDamage()), WidthLimitedOutputStream.BOTH);
-                        
-                    }
-                    
-                    commandParser.getWorld().getPlayer().setHealth(commandParser.getWorld().getPlayer().getHealth() - commandParser.getWorld().getPlayer().getLocation().getBoss().getDamage());
-                    
+
+                    valid = true;
+
                 }
-                
-                valid = true;
+
+            }
+
+            if(!valid){
+
+                commandParser.getWorld().getOutputStream().printSpaced(commandParser.getWorld().getMessageBuilder().getMoveInvalidMessage(), WidthLimitedOutputStream.BOTH);
 
             }
         
-        }
-        
-        if(!valid){
-            
-            commandParser.getWorld().getOutputStream().printSpaced(commandParser.getWorld().getMessageBuilder().getMoveInvalidMessage(), WidthLimitedOutputStream.BOTH);
-            
         }
         
     }
